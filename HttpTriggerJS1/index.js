1b90908c2dd0c7015353a4f170331408fa147e4e
Martinsdarden <martino.leoni@tiscali.it>
2017-05-29 13:09:27 +0200

    Update index.js

--- HttpTriggerJS1/index.js
+++ HttpTriggerJS1/index.js
@@ -1,11 +1,11 @@
-
+ies
 module.exports = function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
     if (req.query.name || (req.body && req.body.name)) {
         context.res = {
             // status: 200, /* Defaults to 200 */
-            body: "Hello " + (req.query.name || req.body.name)
+            bodies: "Hello " + (req.query.name || req.body.name)
         };
     }
     else {
